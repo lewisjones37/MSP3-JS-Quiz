2c7ef593b5c7ea3c82a2ce2b82ee8d7f0359ecc2
lewisjones37 <>
2023-05-09 13:49:15 +0000

    test commit following issues stated on slack

--- game.js
+++ game.js
@@ -56,7 +56,7 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS ) {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS ) {
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('/end.html')
@@ -64,14 +64,14 @@ getNewQuestion = () => {
 
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.getElementsByClassName.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
-        const number = choicedataset['number']
+        const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
     })
 
